800b00a07d84d302f3433618a35737fe591efd2c
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2021-12-28 03:02:57 +0530

    Some improve

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,10 +85,32 @@ let swiper = new Swiper('.portfolio__container', {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+      const scrollY = window.pageYOffset
+
+      sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight
+            const sectionTop = current.offsetTop - 50;
+            sectionId = current.getAttribute('id')
+
+            if(scrollY > sectionTop && scrollY <=sectionTop + sectionHeight){
+                  document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.add('active-link')
+            }
+            else{
+                  document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active-link')
+            }
+      })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-
+function scrollHeader(){
+      const nav = document.getElementById('header')
+      if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/
 
