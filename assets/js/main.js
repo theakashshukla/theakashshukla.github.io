f43fde111a5e22267702355eb831e1b7ea38242f
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-02-20 20:26:55 +0530

    Final

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,18 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*==================== DARK LIGHT THEME ====================*/
+
+/*==================== change text ====================*/
+var text = ["Ethical Hacker 🛡️", "App Developer 📲", "Software Engineer 🧑‍💻", ];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 10000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
