a2cd37ba0f27c09beb9722650c0cf3d745338ac8
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-12-02 03:58:53 +0530

    Fix bug in login functionality

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,100 +1,63 @@
-import clsx from 'clsx';
+// import Link from "next/link"
 
-import { GitHubIcon, TwitterIcon } from '@/components/Icons';
-import NavIcon from '@/components/navigations/NavIcon';
-import NavIconQuickAccess from '@/components/navigations/NavIconQuickAccess';
-import NavLink from '@/components/navigations/NavLink';
-import NavLinkDropdown from '@/components/navigations/NavLinkDropdown';
-import NavLinkExpanded from '@/components/navigations/NavLinkExpanded';
-import NavLogo from '@/components/navigations/NavLogo';
+// import { siteConfig } from "@/config/site"
+// import { cn } from "@/lib/utils"
+// import { CommandMenu } from "@/components/command-menu"
+import { Icons } from "@/components/Icons"
+// import { MainNav } from "@/components/main-nav"
+// import { MobileNav } from "@/components/mobile-nav"
+import { ModeToggle } from "@/components/mode-toggle"
+import { buttonVariants } from "@/components/ui/button"
 
-import useOnScroll from '@/hooks/useOnScroll';
-
-const workLinks = [
-  { title: 'Skills & Tools', href: '/work/skills-and-tools' },
-  { title: 'Experience', href: '/work/experience' },
-  { title: 'Studio', href: '/work/studio' },
-  { title: 'Contact', href: '/work/contact' },
-];
-
-function Navbar() {
-  const isScrolled = useOnScroll(0);
-
+export function SiteHeader() {
   return (
-    <header
-      className={clsx('fixed top-0 right-0 left-0 z-[1000]', 'fm:absolute')}
-    >
-      <div
-        className={clsx(
-          'fixed inset-0 h-16',
-          [
-            isScrolled === true && [
-              'border-divider-light border-b bg-white/70 backdrop-blur',
-              'dark:border-divider-dark dark:bg-slate-900/80',
-            ],
-          ],
-          'fm:hidden'
-        )}
-      />
-      <div className={clsx('h-2', [isScrolled === true && ['-mt-2']])} />
-      <div className={clsx('content-wrapper-max')}>
-        <div
-          className={clsx(
-            'relative z-50 flex h-16 items-center justify-between px-2 text-sm',
-            'md:px-4'
-          )}
-        >
-          <nav className={clsx('flex', 'md:gap-2')} data-accent="violet">
-            <NavLogo href="/" title="Home" />
-            <ul className={clsx('flex items-center', 'md:gap-1')}>
-              <li>
-                <NavLink title="Projects" href="/projects" />
-              </li>
-              <li>
-                <NavLink title="Blog" href="/blog" />
-              </li>
-              <li>
-                <NavLink title="T.I.L" href="/today-i-learned" />
-              </li>
-              <li className={clsx('lg:hidden')} data-accent="blue">
-                <NavLinkDropdown title="Work" items={workLinks} />
-              </li>
-              <li className={clsx('hidden lg:block')} data-accent="blue">
-                <NavLinkExpanded title="Work" items={workLinks} />
-              </li>
-            </ul>
-          </nav>
-          <ul className={clsx('flex items-center')}>
-            <li className={clsx('hidden', 'sm:block')}>
-              <NavIcon
-                href="https://twitter.com/enjidev"
-                icon={<TwitterIcon className={clsx('h-5 w-5')} />}
-                title="Twitter"
-              />
-            </li>
-            <li className={clsx('hidden', 'sm:block')}>
-              <NavIcon
-                href="https://github.com/enjidev"
-                icon={<GitHubIcon className={clsx('h-5 w-5')} />}
-                title="GitHub"
-              />
-            </li>
-            <li className={clsx('hidden', 'sm:block')}>
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center">
+        {/* <MainNav />
+        <MobileNav /> */}
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+          <div className="w-full flex-1 md:w-auto md:flex-none">
+            {/* <CommandMenu /> */}
+          </div>
+          <nav className="flex items-center">
+            {/* <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
               <div
-                className={clsx(
-                  'ml-2 mr-4 h-3 w-[1px] bg-slate-200',
-                  'dark:bg-slate-700'
+                className={cn(
+                  buttonVariants({
+                    variant: "ghost",
+                  }),
+                  "w-9 px-0"
                 )}
-              />
-            </li>
-            <li className={clsx('mr-2')}>
-              <NavIconQuickAccess />
-            </li>
-          </ul>
+              >
+                <Icons.gitHub className="h-4 w-4" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.twitter}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={cn(
+                  buttonVariants({
+                    variant: "ghost",
+                  }),
+                  "w-9 px-0"
+                )}
+              >
+                <Icons.twitter className="h-3 w-3 fill-current" />
+                <span className="sr-only">Twitter</span>
+              </div>
+            </Link> */}
+            <ModeToggle />
+          </nav>
         </div>
       </div>
     </header>
-  );
+  )
 }
-
-export default Navbar;
