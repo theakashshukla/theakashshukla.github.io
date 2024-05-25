303951fca8dff6a3b4f866503a21f6d4b76f003f
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2024-05-26 03:20:44 +0530

    chore: Update Icons component with lucide-react icons

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -11,58 +11,5 @@ export type Icon = LucideIcon;
 export const Icons = {
   sun: SunMedium,
   moon: Moon,
-  twitter: Twitter,
 
-  twitx: ({ ...props }: LucideProps) => (
-    <svg
-      aria-hidden="true"
-      focusable="false"
-      data-prefix="fab"
-      height="1.5rem"
-      data-icon="twitx"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 448 512"
-      {...props}
-    >
-      <path
-        fill="currentColor"
-        d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
-      />
-    </svg>
-  ),
-
-  linkedin: (props: LucideProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="1.5rem"
-      viewBox="0 0 24 24"
-      {...props}
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    >
-      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-      <rect width="4" height="12" x="2" y="9" />
-      <circle cx="4" cy="4" r="2" />
-    </svg>
-  ),
-  github: (props: LucideProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="1.5rem"
-      viewBox="0 0 24 24"
-      {...props}
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    >
-      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-      <path d="M9 18c-4.51 2-5-2-7-2" />
-    </svg>
-  ),
 };
--- components/page/Header.tsx
+++ components/page/Header.tsx
@@ -51,19 +51,19 @@ export default function Header() {
                 <a href="#_" className="duration-200 hover:text-gray-400">
                   {/* Twitter */}
                   <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset size ring-yellow-600/20">
-                    <Icons.twitx className="size-6" />
+                    <Icons.moon className="size-6" />
                   </span>
                 </a>
                 <a href="#_" className="duration-200 hover:text-gray-400">
                   {/* Linkedin */}
                   <span className="inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">
-                    <Icons.linkedin className="size-4" />
+                    <Icons.moon className="size-4" />
                   </span>
                 </a>
                 <a href="#_" className="duration-200 hover:text-gray-400">
                   {/* GitHub */}
                   <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                    <Icons.github className="size-4" />
+                    <Icons.moon className="size-4" />
                   </span>
                 </a>
               </div>
